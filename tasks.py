47ca9f37d4e6e9d2857347b72a8273322f8cfa62
Marko <marko.mutavdzic@factoryww.com>
2021-05-13 16:04:44 +0200

    preradjene funkcije, gotove

--- tasks.py
+++ tasks.py
@@ -1,81 +1,83 @@
 from movies import movies
 
+
 # Write a function that takes a single movie and
 # returns True if its IMDB score is above 5.5
 
 
-def movie_score_above_55(single_movie):
-    return single_movie["imdb"] > 5.5
-
-    # list_of_movies = []
-    # for movie in movies:
-    #     if movie['imdb'] > 5.5:
-    #         movie_above = movie['name'], True
-    #         list_of_movies.append(movie_above)
-    # return list_of_movies
+def movie_score_above_55(movie_name):
+    for movie in movies:
+        if movie['name'] == movie_name:
+            return movie['imdb'] > 5.5
+    return "No such movie"
 
 
 # Write a function that returns a sublist of movies
 # with an IMDB score above 5.5.
 
-def high_score_movies(movies):
-    return [movie['name'] for movie in movies if movie_score_above_55(movie)]
 
-# def high_score_movies_collection():
-#     list_of_high_score_movies = []
-#     for movie in movies:
-#         if movie_score_above_55(movie):
-#             list_of_high_score_movies.append(movie)
-#     return list_of_highs_core_movies
+def list_movies_with_score_above_55():
+    list_movies = []
+    for movie in movies:
+        if movie['imdb'] > 5.5:
+            list_movies.append(movie['name'])
+    # list_movies2 = [movie['name'] for movie in movies if movie['imdb'] > 5.5]
+    return list_movies
 
-
-# def list_movies_with_score_above_55():
-#     list_movies = []
-#     for movie in movies:
-#         if movie['imdb'] > 5.5:
-#             list_movies.append(movie['name'])
-#     return list_movies
 
 # Write a function that takes a category name and returns
 # just those movies under that category.
 
-# kako ovu funkciju uraditi elegantnije
-def category_movies(movies):
-    categories_movies = {}
+
+def category_movies(category):
+    category_movies = []  # pricali smo da bude set ali mi je lakse bilo da koristim listu
     for movie in movies:
-        if movie['category'] not in categories_movies:
-            categories_movies[movie['category']] = []
-        categories_movies[movie['category']].append(movie['name'])
-    return categories_movies
-
-
+        if movie['category'] == category:
+            category_movies.append(movie)
+    # category_movies2=[movie for movie in movies if movie['category'] == category]
+    if not category_movies:
+        return "No such category"
+    return category_movies
 
 
 # Write a function that takes a list of movies and computes
 # the average IMDB score.
 
-def average_score(movies):
-    scores = [movie["imdb"] for movie in movies]
-    avg_score = (round(sum(scores) / len(scores), 2))
-    return f'Average IMDB score is: {avg_score}'
+def average_score(list_of_movies):
+    scores = []
+    dict_movies = [movie['name'] for movie in movies]
+    unknown_movies = []
+    for input_movie in list_of_movies:
+        if input_movie not in dict_movies:
+            unknown_movies.append(input_movie)
+        else:
+            for movie in movies:
+                if movie['name'] == input_movie:
+                    scores.append(movie['imdb'])
+    if not unknown_movies:
+        avg_score = round(sum(scores) / len(scores), 2)
+        return avg_score
+    return f"No movie(s):{unknown_movies}"
 
-    # for movie in movies:
-    #     scores.append(movie['imdb'])
 
 # Write a function that takes a category and computes
 # the average IMDB score (HINT: reuse the function
-# from question 2.
+# from question 3.
 
-# def category_avg_score():
+def category_average_score(category):
+    category_movies = [movie for movie in movies if movie['category'] == category]
+    if not category_movies:
+        return "No such category"
+    else:
+        scores = []
+        for movie in category_movies:
+            scores.append(movie["imdb"])
+        avg_score = round(sum(scores) / len(scores), 2)
+        return avg_score
 
-# kako ovu funkciju uraditi elegantnije
-def category_average_score(movies):
-    categories_score = {}
-    categories_avg_score = {}
-    for movie in movies:
-        if movie['category'] not in categories_score:
-            categories_score[movie['category']] = []
-        categories_score[movie['category']].append(movie['imdb'])
-        for k, v in categories_score.items():
-            categories_avg_score.update({k: round(sum(v) / len(v),2)})
-    return categories_avg_score
+
+
+
+
+
+
